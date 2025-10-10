d45ad6e29867e4a82b785e158e89dee7aaedf03b
Arinze <francisokoye48@gmail.com>
2025-10-10 23:20:53 +0100

    creating main landing page of the website

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -98,6 +98,12 @@ HTML;
                             <input type="text" placeholder="What do you want to ask or share?" style="width:100%" class="input"/>
                         </div>
                     </div>
+                    <div style="display:flex">
+                        <div><i class="fa fa-question-circle-o"></i> Ask</div>
+                        <div><i class="fa fa-edit"></i> Answer</div>
+                        <div><i class="fa fa-pencil"></i> Post</div>
+                        <div></div>
+                    </div>
                 </div>
                 <!-- .main_body ends -->
 HTML;
