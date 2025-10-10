e055aaea46d99b2db5003fd2e349aa616f5a48cd
Arinze <francisokoye48@gmail.com>
2025-10-10 23:00:06 +0100

    creating main landing page of the website

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -94,7 +94,7 @@ HTML;
                         <div style="width:30px;height:30px;border:1px solid #888;border-radius:100%">
                             <img src="/static/images/profile.png" style="width:27px;height:27px;border-radius:100%;margin:1px 0 0 1px"/>
                         </div>
-                        <div style="width:70%;margin-left:6px">
+                        <div style="width:80%;margin-left:6px">
                             <input type="text" placeholder="What do you want to ask or share?" style="width:100%" class="input"/>
                         </div>
                     </div>
