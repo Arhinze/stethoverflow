e4fc641b0d280f515ca9242f6d93aac219c9df10
Arinze <francisokoye48@gmail.com>
2025-10-10 22:20:37 +0100

    creating main landing page of the website

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -91,7 +91,7 @@ HTML;
                 <!-- .main_body starts -->
                 <div class="main_body">
                     <div style="display:flex;justify-contents:space-between;background-color:#fff">
-                        <div style="width:100px;height:100px;border:1px solid #888;border-radius:100%">
+                        <div style="width:15px;height:15px;border:1px solid #888;border-radius:100%">
                             <img src="/static/images/profile.png"/>
                         </div>
                         <div style="width:70%;">
