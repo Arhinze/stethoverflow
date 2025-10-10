b1f8583c704a588ef6811497f29b955f03e4dc65
Arinze <francisokoye48@gmail.com>
2025-10-10 23:55:33 +0100

    creating main landing page of the website

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -101,7 +101,7 @@ HTML;
                             </div>
                         </div>
     
-                        <div style="display:flex;justify-content:space-around;margin-top:9px;padding-bottom:0">
+                        <div style="display:flex;justify-content:space-around;margin-top:9px;">
                             <div><i class="fa fa-question-circle-o"></i> Ask</div>
                             <div><i class="fa fa-edit"></i> Answer</div>
                             <div><i class="fa fa-pencil"></i> Post</div>
