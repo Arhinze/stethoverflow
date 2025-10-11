9001302a9aea12b2f3d1c2063f1a83828cd58207
Arinze <francisokoye48@gmail.com>
2025-10-11 08:55:26 +0100

    creating main landing page of the website

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -112,7 +112,7 @@ HTML;
 
                     <div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div> 
 
-                    <div style="display:flex">
+                    <div style="display:flex;margin:12px 6px;padding-bottom:12px">
                         <div style="width:30px;height:30px;border:2px solid #d6e3fd;border-radius:100%">
                             <a href="$site_url/static/images/profile.png"><img src="/static/images/profile.png" style="width:27px;height:27px;border-radius:100%;margin:1.35px 0 0 1.35px"/></a>
                         </div>
