cd6370cc0b01cd5f3976f3f8f67131e2e930f27e
Arinze <francisokoye48@gmail.com>
2025-10-11 16:50:24 +0100

    creating main landing page of the website

--- drafts.php
+++ drafts.php
@@ -6,7 +6,7 @@ Index_Segments::header();
 <div class="main_body">
     <div style="display:flex;margin-left:15px">
         <div style="margin-top:18px;margin-left:6px"><a href="/questions" style="color:#888">For you</a></div>
-        <div style="border-bottom:2px solid #2b8eeb;padding:15px 6px;margin-left:6px"><a href="/questions" style="color:#2b8eeb">Drafts</a></div>
+        <div style="border-bottom:2px solid #2b8eeb;padding:15px 6px;margin-left:6px"><a href="/drafts" style="color:#2b8eeb">Drafts</a></div>
         <!-- <div>AI Questions</div> -->
     </div>
 </div>
