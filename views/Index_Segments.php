0a2a73f5730b684cd68984e4b46f252c84dce9ed
Arinze <francisokoye48@gmail.com>
2025-10-11 15:01:34 +0100

    creating main landing page of the website

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -344,8 +344,8 @@ HTML;
                         <div class="questions" style="margin-bottom:3px"><h4>What is the most bizarre and rare syndrome you've personally encountered or read about?</h4></div>
 
                         <div style="display:flex">
-                            <div style="border:1px solid #888;padding:12px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
-                            <div style="margin-left:15px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle"></i> 3</div>
+                            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+                            <div style="margin-left:12px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle"></i> 3</div>
                         </div>
                     </div><!-- .posts_and_questions_div ends -->
 
