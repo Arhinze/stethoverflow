5f717f80e43baa8eb07e159757b0fbe7b0895a70
Arinze <francisokoye48@gmail.com>
2025-10-11 16:35:41 +0100

    creating main landing page of the website

--- questions.php
+++ questions.php
@@ -5,8 +5,8 @@ Index_Segments::header();
 
 <div class="main_body">
     <div style="display:flex">
-        <div style="border-bottom:2px solid #2b8eeb"><a href="/questions" style="color:#2b8eeb">For you</a></div>
-        <div><a href="/drafts" style="color:#888">Drafts</a></div>
+        <div style="border-bottom:2px solid #2b8eeb;padding:15px 12px"><a href="/questions" style="color:#2b8eeb">For you</a></div>
+        <div style="margin:12px"><a href="/drafts" style="color:#888">Drafts</a></div>
         <!-- <div>AI Questions</div> -->
     </div>
 </div>
