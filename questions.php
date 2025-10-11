2bcf93e63b3e66f6d8a7763913ea6f4401984fc0
Arinze <francisokoye48@gmail.com>
2025-10-11 17:31:35 +0100

    creating main landing page of the website

--- questions.php
+++ questions.php
@@ -3,13 +3,115 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
 Index_Segments::header();
 ?>
 
-<div class="main_body">
+<div class="main_body"><!-- .main_body starts -->
     <div style="display:flex;margin-left:15px">
         <div style="border-bottom:2px solid #2b8eeb;padding:15px 6px"><a href="/questions" style="color:#2b8eeb">For you</a></div>
         <div style="margin-left:12px;margin-top:18px"><a href="/drafts" style="color:#888">Drafts</a></div>
         <!-- <div>AI Questions</div> -->
     </div>
-</div>
+
+    <!-- Question 1 -->
+    <!-- .posts_and_questions_div starts -->
+    <div class="posts_and_questions" style="margin:12px 6px">
+        <div class="questions" style="margin-bottom:3px;display:flex">
+            <h4 style="width:90%">What's one medical myth patients still strongly believe in your country?</h4>
+            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
+        </div>
+
+        <!-- .below_only_questions start -->
+        <div class="below_only_questions" style="display:flex">
+            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
+        </div><!-- .below_only_questions end -->
+    </div><!-- .posts_and_questions_div ends -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+
+    <!-- Question 2 -->
+    <!-- .posts_and_questions_div starts -->
+    <div class="posts_and_questions" style="margin:12px 6px">
+        <div class="questions" style="margin-bottom:3px;display:flex">
+            <h4 style="width:90%">Which year of medical school is the hardest — and why?</h4>
+            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
+        </div>
+
+        <!-- .below_only_questions start -->
+        <div class="below_only_questions" style="display:flex">
+            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
+        </div><!-- .below_only_questions end -->
+    </div><!-- .posts_and_questions_div ends -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+
+    <!-- Question 3 -->
+    <!-- .posts_and_questions_div starts -->
+    <div class="posts_and_questions" style="margin:12px 6px">
+        <div class="questions" style="margin-bottom:3px;display:flex">
+            <h4 style="width:90%">If you could redesign medical school, what's one thing you'd change?</h4>
+            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
+        </div>
+
+        <!-- .below_only_questions start -->
+        <div class="below_only_questions" style="display:flex">
+            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
+        </div><!-- .below_only_questions end -->
+    </div><!-- .posts_and_questions_div ends -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+
+    <!-- Question 4 -->
+    <!-- .posts_and_questions_div starts -->
+    <div class="posts_and_questions" style="margin:12px 6px">
+        <div class="questions" style="margin-bottom:3px;display:flex">
+            <h4 style="width:90%">Can AI replace radiologists and pathologists in the next decade?</h4>
+            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
+        </div>
+
+        <!-- .below_only_questions start -->
+        <div class="below_only_questions" style="display:flex">
+            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
+        </div><!-- .below_only_questions end -->
+    </div><!-- .posts_and_questions_div ends -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+
+    <!-- Question 5 -->
+    <!-- .posts_and_questions_div starts -->
+    <div class="posts_and_questions" style="margin:12px 6px">
+        <div class="questions" style="margin-bottom:3px;display:flex">
+            <h4 style="width:90%">Which medical specialties are most at risk of being replaced by AI in the next 10 years?</h4>
+            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
+        </div>
+
+        <!-- .below_only_questions start -->
+        <div class="below_only_questions" style="display:flex">
+            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
+        </div><!-- .below_only_questions end -->
+    </div><!-- .posts_and_questions_div ends -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+
+    <!-- Question 6 -->
+    <!-- .posts_and_questions_div starts -->
+    <div class="posts_and_questions" style="margin:12px 6px">
+        <div class="questions" style="margin-bottom:3px;display:flex">
+            <h4 style="width:90%">What's the most underrated medical innovation in the last decade?</h4>
+            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
+        </div>
+
+        <!-- .below_only_questions start -->
+        <div class="below_only_questions" style="display:flex">
+            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
+            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
+        </div><!-- .below_only_questions end -->
+    </div><!-- .posts_and_questions_div ends -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+</div><!-- .main_body ends -->
 
 <?php
 Index_Segments::footer();
