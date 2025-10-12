d53d3fdb33d1788bac0d2876abcc4ebc38120f82
Arinze <francisokoye48@gmail.com>
2025-10-12 16:01:32 +0100

    creating spaces page

--- spaces.php
+++ spaces.php
@@ -63,7 +63,7 @@ Index_Segments::header();
                     <div style="text-align:center;padding:0 12px 15px 12px">The Eyes of Medicine. Can you make a diagnosis from these x-ray images?</div>
                 </div>
 
-                <div style="position:absolute;height:36%;width:36%;border-radius:9px;border:3px solid #2b8eeb;top:10%;left:14%"><img src="/static/images/spaces_profile_photo1.png" style="width:100%;height:auto;border-radius:9px"/></div>
+                <div style="position:absolute;height:36%;width:36%;border-radius:9px;border:3px solid #2b8eeb;top:15%;left:20%"><img src="/static/images/spaces_profile_photo1.png" style="width:100%;height:auto;border-radius:9px"/></div>
             </div>
         </div>
 
