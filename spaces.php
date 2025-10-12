a4e6e3724644c021400aa4d8a433c3922cedaae2
Arinze <francisokoye48@gmail.com>
2025-10-12 14:15:20 +0100

    creating questions page

---
+++ spaces.php
@@ -0,0 +1,63 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+Index_Segments::header();
+?>
+
+<div class="main_body">
+    <!-- .spaces start -->
+    <div class="your_spaces">
+        <div style="border-bottom:2px solid #2b8eeb;padding:15px 0"><h3>Your Spaces</h3></div>
+        
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space1.png"/> &nbsp; <b>Medical Robotics</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Surgery</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Anatomy</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Biochemistry</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Pharmacology</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Bioelectronics</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Medical Wearables and IOT in Medicine</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+
+        <div style="display:flex;justify-content:space-between;padding:12px;border-bottom:1px solid #888">
+            <div><img src="/static/images/space2.png"/> &nbsp; <b>Internal Medicine</b></div>
+            <div><i class="fa fa-angle-right"></i></div>
+        </div>
+    </div><!-- .spaces end -->
+
+    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
+
+    <div class="discover_spaces">
+        <h3>Discover Spaces</h3>
+
+        
+    </div>
+</div>
+
+<?php
+Index_Segments::footer();
+?>
