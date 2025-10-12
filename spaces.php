e76a28fec4a3ff4ca7a92642fca66b73ad09dd75
Arinze <francisokoye48@gmail.com>
2025-10-12 15:25:28 +0100

    creating spaces page

--- spaces.php
+++ spaces.php
@@ -56,7 +56,7 @@ Index_Segments::header();
 
         <div class="discover_spaces_div_container">
             <div class="discover_spaces_div" style="position:relative;border-radius:9px;border:1px solid #888;width:41%;height:fit-content">
-                <div style="height:15%;width:100%;overflow:hidden"><img src="/static/images/spaces_cover_photo1.png" style="width:auto;height:100%;border-radius:9px 9px 0 0"/></div>
+                <div style="height:15%;width:100%;overflow:hidden"><img src="/static/images/spaces_cover_photo1.png" style="width:auto;height:15%;border-radius:9px 9px 0 0"/></div>
                 <div style="position:absolute;height:18%;width:18%;border-radius:9px;border:1px solid #fff;top:8%;left:32%"><img src="/static/images/spaces_profile_photo1.png" style="width:100%;height:auto;border-radius:9px"/></div>
                 <div style="height:85%;width:100%;background-color:#fff">
                     <div style="text-align:center;padding:15px 12px"><b>Radiography</b></div>
