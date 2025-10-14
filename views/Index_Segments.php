233ea5f8d778ab58f20e7e13cee47a3e0ddd43f1
Arinze <francisokoye48@gmail.com>
2025-10-14 16:24:35 +0100

    creating ask_or_post section

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -532,16 +532,16 @@ HTML;
                 }
             }
 
-            function alternat9e_add_question_and_create_post(){
-                if(document.getElementById('add_question').style.display == "none") {
+            function alternate_add_question_and_create_post(){
+                //if(document.getElementById('add_question').style.display == "none") {
                     document.getElementById('create_post').style.display = "block";
                     //document.getElementById('aq_text').style = "border-bottom:2px solid #2b8eeb";
                     //document.getElementById('cp_text').style = "border:0";
-                } else if (document.getElementById('add_question').style.display == "block") {
+                //} else if (document.getElementById('add_question').style.display == "block") {
                     document.getElementById('create_post').style.display = "none";
                     //document.getElementById('cp_text').style = "border-bottom:2px solid #2b8eeb";
                     //document.getElementById('aq_text').style = "border:0";
-                }
+                //}
             }                                                                            
             const collection = document.getElementsByClassName("invalid");
                                                                                                  
