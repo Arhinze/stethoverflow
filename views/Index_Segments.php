8a5ad9690d17f509244696a59f03289980ee6341
Arinze <francisokoye48@gmail.com>
2025-10-14 18:47:15 +0100

    adding user sign in feature

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -40,11 +40,11 @@ class Index_Segments{
                 </div>
             </div><!-- end of 2nd .headers --> 
             <!-- .join_us starts -->
-            <div class="join_us" id="join_us" style="position:fixed;top:60px;right:15px;z-index:10;line-height:60px;background-color:#fff;border-radius:6px;box-shadow:0 0 3px 0 #d6e2fb;text-align:center;">
+            <div class="join_us" id="join_us" style="position:fixed;top:60px;right:15px;z-index:10;line-height:50px;background-color:#fff;border-radius:6px;box-shadow:0 0 3px 0 #d6e2fb;text-align:center;">
                 <div style="position:relative">
                     <div style="position:absolute;float:right;right:12px;top:12px" onclick="show_div('join_us')"><i class="fa fa-times"></i></div>
                 </div>
-                <div style="font-weight:bold">
+                <div style="font-weight:bold;margin-top:18px">
                     Join $site_name
                 </div>
                 <div>
