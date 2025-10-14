a30dd5729ff3120b48dfec1b387e9993d68adf44
Arinze <francisokoye48@gmail.com>
2025-10-14 01:41:55 +0100

    creating comment section

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -152,11 +152,13 @@ HTML;
                     </div><!-- .posts_and_questions_div ends -->
 
                     <!-- .add_comment starts -->
-                    <div id="add_comment" style="display:block" class="add_comment" onclick="alternate_comment_div()">
-                        <div class="profile_image_div" style="border:1px solid #fff">
-                            <a href="/static/images/profile.png"><img src="/static/images/profile.png" class="profile_image"/></a>
+                    <div style="display:block" class="add_comment" id="add_comment" onclick="alternate_comment_div()">
+                        <div style="display:flex">
+                            <div class="profile_image_div" style="border:1px solid #fff">
+                                <a href="/static/images/profile.png"><img src="/static/images/profile.png" class="profile_image"/></a>
+                            </div>
+                            <div class="input" style="background-color:#fff;color:#888;width:70%;margin-left:12px">Add a comment...</div>
                         </div>
-                        <div class="input" style="background-color:#fff;color:#888;width:70%;margin-left:12px">Add a comment...</div>
                     </div>
                     <!-- .add_comment ends -->
 
