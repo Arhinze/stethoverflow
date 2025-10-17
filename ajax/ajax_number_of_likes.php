9a8e48e37daeee02a341ef3c6fe22ec78eb01b17
Arinze <francisokoye48@gmail.com>
2025-10-18 00:39:09 +0100

    adding pop_up for sign in for logged out users

--- ajax/ajax_number_of_likes.php
+++ ajax/ajax_number_of_likes.php
@@ -28,6 +28,23 @@ if($data) {
         
     }
 } else {
-    echo "<div class='pop_up'>kindly login to continue</div>";
+    echo <<<HTML
+        <div class='invalid' style='background-color:#2b8eeb;color:#fff;top:30%;left:15%;width:70%;padding:18px'>
+            <div><b>kindly login to continue</b></div>
+            <div>    
+                <div style="text-align:center;font-size:27px;margin-top:30px;margin-bottom:-16px;font-family:Dancing Script">Welcome to</div>
+                <h1 style="font-size:30px;text-align:center">Steth<span style="color: #2b8eeb">Overflow</span></h1>
+                <div style="color:#2b8eeb;font-size:15px;text-align:center;margin-top:-19px"><i class="fa fa-key"></i>&nbsp; All data is encrypted</div>
+            </div>
+        
+            <div class="input" style="border-radius:36px;text-align:center;background-color:blue;border:1px solid blue;font-weight:bold;color:#fff;margin-top:30px">
+                <a href="/auth/google-login.php" style="color:#fff;padding:12px 21%"><i class="fa fa-google" id="signinButton"></i>&nbsp; Continue with Google</a>
+            </div>
+
+            <div class="input" style="border-radius:36px;text-align:center;background-color:blue;border:1px solid blue;font-weight:bold;color:#fff;margin-top:30px">
+                <a href="/login.php" style="color:#fff;padding:12px 21%"><i class="fa fa-google" id="signinButton"></i>&nbsp; Login/Sign up</a>
+            </div>
+        </div>
+HTML;
 }
 ?>
