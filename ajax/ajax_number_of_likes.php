9e170c80a23f573f2f8a37c576ed108ee183f51e
Arinze <francisokoye48@gmail.com>
2025-10-18 00:57:06 +0100

    adding pop_up for sign in for logged out users

--- ajax/ajax_number_of_likes.php
+++ ajax/ajax_number_of_likes.php
@@ -29,23 +29,23 @@ if($data) {
     }
 } else {
     echo <<<HTML
-        <div class='invalid' id="invalid_sign_in" style='background-color: #d6e2fb;color:#fff;top:30%;left:15%;height:fit-content;width:70%;padding:18px'>
+        <div class='invalid' id="invalid_sign_in" style='background-color: #2b8eeb;color:#fff;top:30%;left:15%;height:fit-content;width:70%;padding:18px'>
             <div style="display:flex;justify-content:space-between">
                 <div><b>kindly login to continue</b></div>
                 <div onclick="show_div('invalid_sign_in')"><i class="fa fa-times"></i></div>
             </div>
             <div>    
                 <div style="text-align:center;font-size:27px;margin-top:30px;margin-bottom:-16px;font-family:Dancing Script">Welcome to</div>
-                <h1 style="font-size:30px;text-align:center">Steth<span style="color: #2b8eeb">Overflow</span></h1>
-                <div style="color:#2b8eeb;font-size:15px;text-align:center;margin-top:-19px"><i class="fa fa-key"></i>&nbsp; All data is encrypted</div>
+                <h1 style="font-size:30px;text-align:center">Steth<span style="color: #d6e2fb">Overflow</span></h1>
+                <div style="color: #d6e2fb;font-size:15px;text-align:center;margin-top:-19px"><i class="fa fa-key"></i>&nbsp; All data is encrypted</div>
             </div>
         
-            <div class="input" style="border-radius:36px;text-align:center;background-color:blue;border:1px solid blue;font-weight:bold;color:#fff;margin-top:30px">
+            <div class="input" style="border-radius:36px;text-align:center;background-color:#d6e2fb;border:1px solid blue;font-weight:bold;color:#fff;margin-top:30px">
                 <a href="/auth/google-login.php" style="color:#fff;padding:12px 21%"><i class="fa fa-google" id="signinButton"></i>&nbsp; Continue with Google</a>
             </div>
 
             <div class="input" style="border-radius:36px;text-align:center;background-color:blue;border:1px solid blue;font-weight:bold;color:#fff;margin-top:30px">
-                <a href="/login.php" style="color:#fff;padding:12px 21%"><i class="fa fa-google" id="signinButton"></i>&nbsp; Login/Sign up</a>
+                <a href="/login.php" style="color:#fff;padding:12px 21%"> &nbsp; Login/Sign up</a>
             </div>
         </div>
 HTML;
