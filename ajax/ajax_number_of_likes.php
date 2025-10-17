affc4dfa0145bdc467288d4bd6cc5c87ab07f06a
Arinze <francisokoye48@gmail.com>
2025-10-17 22:44:21 +0100

    adding functional likes feature to comment section

--- ajax/ajax_number_of_likes.php
+++ ajax/ajax_number_of_likes.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"]."/php/connection.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/php/account-manager.php");
 
 if(isset($_GET["post_id"])){
     $post_id = str_replace("post", "", htmlentities($_GET["post_id"]));
@@ -10,6 +10,16 @@ if(isset($_GET["post_id"])){
     $like_data = $like_stmt->fetch(PDO::FETCH_OBJ);
 
     $number_of_likes = explode(";", $like_data->likes);
-    echo count($number_of_likes)-1;
+    if(in_array($data->user_id, $number_of_likes)){
+        unset($number_of_likes["$data->user_id"]);
+        echo count($number_of_likes)-1;//adding -1 to balance out the extra array counted from explode() function
+    } else {
+        $update_like_stmt = $pdo->prepare("UPDATE posts SET likes = ? WHERE post_id = ?");
+        $update_like_stmt->execute([$like_data->likes.$data->user_id.";", $post_id]);
+
+        echo count($number_of_likes)-1;//adding -1 to balance out the extra array counted from explode() function
+    }
+
+    
 }
 ?>
