81455decdb6d4720c44c9a76055c5f47bce654c1
Arinze <francisokoye48@gmail.com>
2025-10-17 23:01:13 +0100

    adding functional likes feature to comment section

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -256,7 +256,7 @@ HTML;
                         <!-- .like,comment and share icons start -->
                         <div class="like_comment_and_share_icons">
                             <div class="" style="display:flex">
-                                <div><span id="post$post_d->post_id" style="color:grey" onclick="like_post('post$post_d->post_id')"><i class="fa fa-heart-o"></i></span> <span id="no_of_likes_of_post$post_d->post_id">10</span></div>
+                                <div><span id="post$post_d->post_id" style="color:grey" onclick="like_post('post$post_d->post_id')"><i class="fa fa-heart-o"></i></span> <span id="no_of_likes_of_post$post_d->post_id">0</span></div>
                                 <div style="margin-left:10px"><i class="fa fa-comment-o" onclick="show_div('add_comment$post_d->post_id')"></i> <span id="no_of_comments$post_d->post_id">9</span></div>
                                 <div style="margin-left:10px"><i class="fa fa-retweet" onclick="show_div('quote_comment_div$post_d->post_id')"></i> <span id="no_of_quotes$post_d->post_id"> </span></div>
                                 <div style="margin-left:10px"><i class="fa fa-share-alt"></i> <span id="no_of_shares$post_d->post_id"> </span></div>
