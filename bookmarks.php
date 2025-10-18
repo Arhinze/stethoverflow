36f5225c42d6224721535347b6fe66aa26fc4ba5
Arinze <francisokoye48@gmail.com>
2025-10-18 18:29:08 +0100

    adding new pages for user's profile

---
+++ bookmarks.php
@@ -0,0 +1,14 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+Index_Segments::header();
+?>
+
+<div class="main_body">
+    <div style="text-align:center;padding:3%;font-size:15px;margin-top:50px">
+        <i class="fa fa-bookmark"></i> Your bookmarks will appear hear.
+    </div>
+</div>
+
+<?php
+Index_Segments::footer();
+?>
