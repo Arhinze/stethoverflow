cb649e32579809c4221c96e53d6cd5dfbd5f51fa
Arinze <francisokoye48@gmail.com>
2025-10-18 01:29:35 +0100

    adding pop_up for sign in for logged out users

--- ajax/ajax_number_of_likes.php
+++ ajax/ajax_number_of_likes.php
@@ -28,32 +28,6 @@ if($data) {
         
     }
 } else {
-    echo <<<HTML
-        <div class='invalid' id="invalid_sign_in" style='background-color: #2b8eeb;color:#fff;top:25%;left:15%;height:fit-content;width:70%;padding:18px;display:block'>
-            <div style="display:flex;justify-content:space-between">
-                <div><b>kindly login to continue</b></div>
-                <div onclick="show_div('invalid_sign_in')"><i class="fa fa-times"></i></div>
-            </div>
-            <div>    
-                <div style="text-align:center;font-size:27px;margin-top:30px;margin-bottom:-16px;font-family:Dancing Script">Welcome to</div>
-                <h1 style="font-size:30px;text-align:center">Steth<span style="color: #d6e2fb">Overflow</span></h1>
-                <div style="color: #d6e2fb;font-size:15px;text-align:center;margin-top:-19px"><i class="fa fa-key"></i>&nbsp; All data is encrypted</div>
-            </div>
-        
-            <div class="input" style="border-radius:36px;text-align:center;background-color:#d6e2fb;border:1px solid blue;font-weight:bold;margin-top:30px">
-                <a href="/auth/google-login.php" style="color: #2b8eeb;padding:12px 21%"><i class="fa fa-google" id="signinButton"></i>&nbsp; Continue with Google</a>
-            </div>
-
-            <div class="input" style="border-radius:36px;text-align:center;background-color:blue;border:1px solid blue;font-weight:bold;color:#fff;margin-top:15px">
-                <a href="/login.php" style="color:#fff;padding:12px 21%"> &nbsp; Login/Sign up</a>
-            </div>
-        </div>
-
-        <script>
-            close_div(vari) {
-                document.getElementById(vari).style.display = "none";
-            }
-        </script>
-HTML;
+    include_once($_SERVER["DOCUMENT_ROOT"]."/ajax/ajax_signin_popup.php");
 }
 ?>
