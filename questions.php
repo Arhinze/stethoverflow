179f972e6e8841c3c11d635d88126ea0b5be70cc
Arinze <francisokoye48@gmail.com>
2025-10-18 17:20:29 +0100

    caaling up questions on questions page from database

--- questions.php
+++ questions.php
@@ -12,46 +12,6 @@ Index_Segments::header();
 
     <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
 
-    <!-- Question 1 -->
-    <!-- .posts_and_questions_div starts -->
-    <div class="posts_and_questions" style="margin:12px 6px">
-        <div class="questions" style="margin-bottom:3px;display:flex">
-            <h4 style="width:90%">What's one medical myth patients still strongly believe in your country?</h4>
-            <div style="width:6%;margin-top:24px;margin-left:6px;color:#888"><i class="fa fa-times"></i></div>
-        </div>
-
-        <!-- .below_only_questions start -->
-        <div class="below_only_questions" style="display:flex">
-            <div style="border:1px solid #888;padding:6px;border-radius:15px" onclick="show_div('write_answer_div1')"><i class="fa fa-edit"></i>&nbsp; Answer </div>
-            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
-        </div><!-- .below_only_questions end -->
-    </div><!-- .posts_and_questions_div ends -->
-
-    <!-- .write_answer starts -->
-    <div class="write_answer" id="write_answer_div1" style="display:none">
-        <!-- .write_answer_top starts -->
-        <div class="write_answer_top">
-            <div style="font-size:18px;color:#888" onclick="show_div('write_answer_div1')"><b> X </b></div>
-            <div class="button" style="font-size:12px">Post</div>
-        </div><!-- .write_answer_top ends -->
-        <div style="display:flex">
-            <div class="profile_image_div" style="margin-top:5px">
-                <a href="/static/images/profile_new.png"><img src="/static/images/profile_new.png" class="profile_image"/></a>
-            </div>
-            <div style="margin-left:-13px">
-                <div style="font-size:12px"><b>Cassy Maya</b></b></div>
-                <div class="input" style="width:100%;font-size:12px">Orthopaedic Surgeon (2007 - present) &nbsp; <i class="fa fa-angle-down"></i></div>
-            </div>
-        </div>
-        
-        <h4 style="width:90%">What's one medical myth patients still strongly believe in your country?</h4>
-        <div style="color:#888">Add Image + </div>
-        <div class="">
-            <textarea class="textarea" placeholder="Write your answer on this"></textarea>
-        </div>
-    </div><!-- .write_answer ends -->
-    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
-
 <?php
     $search_question = $pdo->prepare("SELECT * FROM questions ORDER BY question_id DESC LIMIT ?, ?");
     $search_question->execute([0,1000]);
@@ -61,10 +21,10 @@ Index_Segments::header();
 ?>
     <!-- Question 1,2,3... -->
     <!-- .posts_and_questions_div starts -->
-    <div class="posts_and_questions" id="pq<?=$sd->question_id?>" style="margin:12px 6px">
+    <div class="posts_and_questions" id="question_id_<?=$sd->question_id?>" style="margin:12px 6px">
         <div class="questions" style="margin-bottom:3px;display:flex">
             <h4 style="width:90%"><?=$sd->title?></h4>
-            <div style="width:6%;margin-top:24px;margin-left:6px;color:#888" onclick="show_div('pq<?=$sd->question_id?>')"><i class="fa fa-times"></i></div>
+            <div style="width:6%;margin-top:24px;margin-left:6px;color:#888" onclick="show_div('question_id_<?=$sd->question_id?>')"><i class="fa fa-times"></i></div>
         </div>
 
         <!-- .below_only_questions start -->
