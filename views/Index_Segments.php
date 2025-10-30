d3c6297d1fbef82bf2decd17a95e90fc1d063854
Arinze <francisokoye48@gmail.com>
2025-10-31 00:26:03 +0100

    editing lower page area of home page

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -430,29 +430,6 @@ HTML;
 HTML;
                 }
   
-
-
-
-
-
-
-            echo <<<HTML
-                    <!-- .posts_and_questions_div starts -->
-                    <div class="posts_and_questions" style="margin:12px 6px">
-                        <div class="questions" style="margin-bottom:3px;display:flex">
-                            <h4 style="width:90%">What is the most bizarre and rare syndrome you've personally encountered or read about?</h4>
-                            <div style="width:6%;margin-top:24px;margin-left:6px"><i class="fa fa-times"></i></div>
-                        </div>
-
-                        <!-- .below_only_questions start -->
-                        <div class="below_only_questions" style="display:flex">
-                            <div style="border:1px solid #888;padding:6px;border-radius:15px"><i class="fa fa-edit"></i>&nbsp; Answer </div>
-                            <div style="margin-left:12px;margin-top:6px"><i class="fa fa-feed"></i>&nbsp; Follow <i class="fa fa-circle" style="font-size:3px"></i> 3</div>
-                        </div><!-- .below_only_questions end -->
-                    </div><!-- .posts_and_questions_div ends -->
-
-                    <!-- demarcation --><div class="demarcation" style="width:100%;height:7px;background-color:#d6e3fd"></div><!-- demarcation -->
-HTML;
  
             
  
