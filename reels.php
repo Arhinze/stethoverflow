b5fec3fda29f1fca6c7bf933a4c09f4468f6cd74
Arinze <francisokoye48@gmail.com>
2026-01-28 12:40:57 +0100

    adding reels section

--- reels.php
+++ reels.php
@@ -1,6 +1,11 @@
 <?php
 include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
 Index_Segments::header();
+
+$reels_query = $pdo->prepare("SELECT * FROM videos ORDER BY video_id DESC LIMIT ?, ?");
+$reels_query->execute([0,100]);
+$reels_data = $reels_query->fetchAll(PDO::FETCH_OBJ);
+
 ?>
 
 <div class="main_body">
